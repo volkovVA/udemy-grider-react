b2202cb87ab0853810a6ec2761972bad7955eb1a
volkovVA <volkov.slava@hotmail.com>
2022-04-01 17:50:58 +0300

    feat: add components

---
+++ components/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from '@faker-js/faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          text="Hello!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          text="Hi! How are you?"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yestarday at 5:00PM"
+          avatar={faker.image.avatar()}
+          text="Good! Thanks"
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
